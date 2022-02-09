8dd75f7c38fe403fb894d9ff0cb0a877ccac96a1
gareth-was-here <gareth@garethveale.dev>
2022-02-09 20:41:11 +0000

    delete start scripts + add run.js + Staker contract

---
+++ scripts/run.js
@@ -0,0 +1,20 @@
+async function main() {
+
+    // We get the contract to deploy
+    const Staker = await hre.ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, Hardhat!");
+  
+    await greeter.deployed();
+  
+    console.log("Greeter deployed to:", greeter.address);
+  }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+runMain()
