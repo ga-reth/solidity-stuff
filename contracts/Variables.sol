948cb942e096ee8177dab069a018194321b6eea1
gareth-was-here <gareth@garethveale.dev>
2022-02-17 19:32:38 +0000

    variables contract

--- contracts/Variables.sol
+++ contracts/Variables.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.0;
 
 contract Variables {
     
+    // state variables
+    string public text = "hello";
+    uint public num = 123;
+
+    function xyz() public {
+        // local vars not saved on chain
+        uint i = 456;
+
+        // globals accessed inside a function
+        uint timestamp = block.timestamp;
+        address sender = msg.sender;
+    }
+
 }
