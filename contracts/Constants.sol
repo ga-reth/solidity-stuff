90f84818423983fd6d880dc60e288a27c32b0e04
gareth-was-here <gareth@garethveale.dev>
2022-02-17 19:41:08 +0000

    constants contract

---
+++ contracts/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Constants {
+    // constants cannot be altered
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
