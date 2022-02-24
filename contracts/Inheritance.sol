6bc0f75ecc69c3b4475490d188c6b1d0968a2d5d
gareth-was-here <gareth@garethveale.dev>
2022-02-24 11:44:14 +0000

    inheritance contract

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -4,5 +4,43 @@ pragma solidity ^0.8.0;
 contract A {
     function foo() public pure virtual returns (string memory) {
         return "A";
+    }   
+}
+
+contract B is A {
+    // override A.foo()
+    function foo() public pure virtual override returns (string memory) {
+        return "B";        
     }
+}
+contract C is A {
+    // override A.foo()
+    function foo() public pure virtual override returns (string memory) {
+        return "C";        
+    }
+}
+
+// multi inheritance is supported
+// parents searched from right to left
+contract D is B, C {
+    //d.foo() returns "C" - right to left
+    function foo() public pure override(B,C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+contract E is C, B {
+    //d.foo() returns "B" - right to left
+    function foo() public pure override(B,C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+
+// inheritance ordered 'most base-like' to 'most derived'
+// swapping will throw compilation error
+contract F is A, B {
+    function foo() public pure override(A,C) returns (string memory) {
+        return super.foo();
+    }    
 }
